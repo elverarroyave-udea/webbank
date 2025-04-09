a6837dd26bdd58d58e086495ed1ccc2d7058592a
Juan Pablo Arango <juanpabloarango@Juans-MacBook-Air.local>
2025-04-09 07:12:21 -0500

    clase de mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Button from '@/components/atoms/Button';
+import NavTitle from '@/components/atoms/NavTitle';
+import Nav from '@/components/organisms/Nav';
+import NavMobile from '@/components/molecules/Navmobile';
 export default function Home() {
-  return <div>Hola Mundo</div>;
+  return (
+    <div className='flex flex-col items-center  min-h-screen bg-black sm:bg-red-700 md:bg-blue-700 lg:bg-green-700 xl:bg-yellow-700'>
+      Hola Mundo
+    </div>
+  );
 }
